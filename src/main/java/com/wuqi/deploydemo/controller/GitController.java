f285afd5db6ae0be730d966aafde13afdc65f3ee
武奇 <risky5200@gmail.com>
2019-08-18 15:12:15 +0800

    执行脚本

--- src/main/java/com/wuqi/deploydemo/controller/GitController.java
+++ src/main/java/com/wuqi/deploydemo/controller/GitController.java
@@ -1,11 +1,13 @@
 package com.wuqi.deploydemo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -18,10 +20,19 @@ import java.util.Map;
 @Slf4j
 public class GitController {
 
+    @Value("${spring.script}")
+    private String script;
+
     @PostMapping("/hook")
     public Map hook(@RequestBody Map body) {
         log.info("git hook info: {}", body);
         // 测试推送代码
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec(script);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return Collections.singletonMap("message", "ok");
     }
 
